6dbda576e0677642b9cbcccbc3454aa13faa7a34
Matt Straathof <matt.straat@gmail.com>
2024-01-17 10:01:47 -0800

    feat: add custom domain name for backing api

--- backend/lambdas/esbuild.ts
+++ backend/lambdas/esbuild.ts
@@ -1,24 +1,25 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import {build} from 'esbuild';
+import * as fs from "fs";
+import * as path from "path";
+import { build } from "esbuild";
 
-const functionsDir = 'src';
-const outDir = 'dist';
+const functionsDir = "src";
+const outDir = "dist";
 const entryPoints = fs
-    .readdirSync(path.join(__dirname, functionsDir))
-    .filter(entry => entry !== 'common')
-    .map(entry => `${functionsDir}/${entry}/handler.ts`);
+  .readdirSync(path.join(__dirname, functionsDir))
+  .filter((entry) => entry !== "common")
+  .filter((entry) => entry !== "shared")
+  .map((entry) => `${functionsDir}/${entry}/handler.ts`);
 
 build({
   entryPoints,
   bundle: true,
   outdir: path.join(__dirname, outDir),
   outbase: functionsDir,
-  platform: 'node',
-  sourcemap: 'external',
+  platform: "node",
+  sourcemap: "external",
   write: true,
   minify: true,
-  tsconfig: './tsconfig.json',
+  tsconfig: "./tsconfig.json",
   keepNames: true,
   // eslint-disable-next-line no-process-exit
 }).catch(() => process.exit(1));
