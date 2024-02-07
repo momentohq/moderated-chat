53d68910e201dfeac3307b884b3ac2e990561317
Pete Gautier <pete@momentohq.com>
2024-02-07 11:13:03 -0800

    fix: set message bar height to avoid post-error scrolling issue

--- android/ModeratedChat/app/src/main/java/software/momento/kotlin/moderatedchat/MainActivity.kt
+++ android/ModeratedChat/app/src/main/java/software/momento/kotlin/moderatedchat/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -385,7 +386,9 @@ fun MessageBar(
     }
 
     Row(
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .height(60.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         if (showImageSizeError) {
