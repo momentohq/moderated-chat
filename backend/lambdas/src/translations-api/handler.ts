9856876470d8684bad4c536a46f62c6a3e40463f
anitarua <anita@momentohq.com>
2024-08-05 12:36:01 -0700

    fix: make sure cache is created in lambda handler

--- backend/lambdas/src/translations-api/handler.ts
+++ backend/lambdas/src/translations-api/handler.ts
@@ -70,6 +70,7 @@ export const createTranslationsApi = async (): Promise<API> => {
     configuration: Configurations.Lambda.latest(),
     defaultTtlSeconds: 60 * 60,
   });
+  await cacheClient.createCache(cacheName);
 
   api.use((req: Request, res: Response, next: NextFunction) => {
     res.cors({});
