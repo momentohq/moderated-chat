fffb27bc3112ce386439ebf1c2d23f63b5ce3d45
anitarua <anita@momentohq.com>
2024-08-20 16:09:01 -0700

    use api gateway url by default, domain name only if hosted zone exists

--- infrastructure/build-and-deploy.sh
+++ infrastructure/build-and-deploy.sh
@@ -12,6 +12,12 @@ rm -f cdk.context.json
 npm i
 npm run build
 
-export API_DOMAIN=${API_DOMAIN:-your-chosen-domain-name.com}
+# For Momento developers, to deploy to your dev account, you will need to have gone through the cell bootstrap
+# process at least once. Alternately, you can use a teammate's endpoint or use the preprod endpoint 
+# (search for Route53 hosted zones in the appropriate AWS accounts).
+# export API_DOMAIN=${API_DOMAIN:-your-chosen-domain-name.com}
+
+# The default is to use the API gateway URL that is provided when you deploy the backend stack.
+export API_DOMAIN="default"
 
 AWS_PROFILE=dev AWS_REGION=us-west-2 npx cdk deploy --require-approval never
--- infrastructure/lib/translation-api.ts
+++ infrastructure/lib/translation-api.ts
@@ -40,58 +40,86 @@ export class TranslationApiStack extends cdk.Stack {
                 : RemovalPolicy.RETAIN,
         });
 
-        // Register the subdomain and create a certificate for it
-        const hostedZone = route53.HostedZone.fromLookup(
-            this,
-            'moderated-chat-api-hosted-zone',
-            {
-                domainName: props.apiDomain,
-            }
-        );
-        const certificate = new certmgr.Certificate(this, 'moderated-chat-api-cert', {
-            domainName: `${props.apiSubdomain}.${props.apiDomain}`,
-            validation: certmgr.CertificateValidation.fromDns(hostedZone),
-        });
-        const defaultRestApiProps: apigw.RestApiProps = {
-            restApiName,
-            endpointTypes: [apigw.EndpointType.REGIONAL],
-            deploy: true,
-            description: "Rest api that contains the backend code/logic for the moderated chat demo",
-            deployOptions: {
-                stageName: 'prod',
-                accessLogDestination: new apigw.LogGroupLogDestination(logGroup),
-                accessLogFormat: apigw.AccessLogFormat.jsonWithStandardFields(),
-                throttlingRateLimit: 10,
-                throttlingBurstLimit: 25,
-                metricsEnabled: true,
-                loggingLevel: MethodLoggingLevel.INFO,
-                description: 'translation endpoint for momento console',
-            },
-            defaultCorsPreflightOptions: {
-                allowOrigins: apigw.Cors.ALL_ORIGINS,
-                allowHeaders: apigw.Cors.DEFAULT_HEADERS,
-                allowMethods: apigw.Cors.ALL_METHODS,
-            },
-            domainName: {
+        // Register the subdomain and create a certificate for it if using a custom domain
+        if (props.apiDomain !== "default") {
+            const hostedZone = route53.HostedZone.fromLookup(
+                this,
+                'moderated-chat-api-hosted-zone',
+                {
+                    domainName: props.apiDomain,
+                }
+            );
+            const certificate = new certmgr.Certificate(this, 'moderated-chat-api-cert', {
                 domainName: `${props.apiSubdomain}.${props.apiDomain}`,
-                endpointType: apigw.EndpointType.REGIONAL,
-                certificate,
-            },
-            cloudWatchRole: true, // allows api gateway to write logs to cloudwatch
-        };
-
-        this.restApi = new apigw.RestApi(this, 'moderated-chat-rest-api', {
-            ...defaultRestApiProps,
-        });
-
-        new route53.ARecord(this, "moderated-chat-rest-api-dns", {
-            zone: hostedZone,
-            recordName: props.apiSubdomain,
-            comment: "This is the A Record used for the moderated chat api backend",
-            target: route53.RecordTarget.fromAlias(
-                new route53Targets.ApiGateway(this.restApi)
-            ),
-        });
+                validation: certmgr.CertificateValidation.fromDns(hostedZone),
+            });
+            const defaultRestApiProps: apigw.RestApiProps = {
+                restApiName,
+                endpointTypes: [apigw.EndpointType.REGIONAL],
+                deploy: true,
+                description: "Rest api that contains the backend code/logic for the moderated chat demo",
+                deployOptions: {
+                    stageName: 'prod',
+                    accessLogDestination: new apigw.LogGroupLogDestination(logGroup),
+                    accessLogFormat: apigw.AccessLogFormat.jsonWithStandardFields(),
+                    throttlingRateLimit: 10,
+                    throttlingBurstLimit: 25,
+                    metricsEnabled: true,
+                    loggingLevel: MethodLoggingLevel.INFO,
+                    description: 'translation endpoint for momento console',
+                },
+                defaultCorsPreflightOptions: {
+                    allowOrigins: apigw.Cors.ALL_ORIGINS,
+                    allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+                    allowMethods: apigw.Cors.ALL_METHODS,
+                },
+                domainName: {
+                    domainName: `${props.apiSubdomain}.${props.apiDomain}`,
+                    endpointType: apigw.EndpointType.REGIONAL,
+                    certificate,
+                },
+                cloudWatchRole: true, // allows api gateway to write logs to cloudwatch
+            };
+    
+            this.restApi = new apigw.RestApi(this, 'moderated-chat-rest-api', {
+                ...defaultRestApiProps,
+            });
+    
+            new route53.ARecord(this, "moderated-chat-rest-api-dns", {
+                zone: hostedZone,
+                recordName: props.apiSubdomain,
+                comment: "This is the A Record used for the moderated chat api backend",
+                target: route53.RecordTarget.fromAlias(
+                    new route53Targets.ApiGateway(this.restApi)
+                ),
+            });
+        } else {
+            const defaultRestApiProps: apigw.RestApiProps = {
+                restApiName,
+                endpointTypes: [apigw.EndpointType.REGIONAL],
+                deploy: true,
+                description: "Rest api that contains the backend code/logic for the moderated chat demo",
+                deployOptions: {
+                    stageName: 'prod',
+                    accessLogDestination: new apigw.LogGroupLogDestination(logGroup),
+                    accessLogFormat: apigw.AccessLogFormat.jsonWithStandardFields(),
+                    throttlingRateLimit: 10,
+                    throttlingBurstLimit: 25,
+                    metricsEnabled: true,
+                    loggingLevel: MethodLoggingLevel.INFO,
+                    description: 'translation endpoint for momento console',
+                },
+                defaultCorsPreflightOptions: {
+                    allowOrigins: apigw.Cors.ALL_ORIGINS,
+                    allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+                    allowMethods: apigw.Cors.ALL_METHODS,
+                },
+                cloudWatchRole: true, // allows api gateway to write logs to cloudwatch
+            };
+            this.restApi = new apigw.RestApi(this, 'moderated-chat-rest-api', {
+                ...defaultRestApiProps,
+            });
+        }
 
         const secretsPath = 'moderated-chat/demo/secrets';
         const v1TranslationApi = new lambda.Function(this, 'moderated-chat-translation-lambda-function', {
